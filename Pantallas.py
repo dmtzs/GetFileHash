ebac5182802d41fee0f59b1e01ff89eb9b3e2020
Diego MS <diego.mtzs00@gmail.com>
2021-02-14 17:25:48 -0600

    Update for README and update for language

--- Pantallas.py
+++ Pantallas.py
@@ -1,19 +1,32 @@
 import os, time
 
 class Menus():
-    def MenuPrin(self, bandera):
+    def MenuPrin(self, bandera, lang):
         os.system("cls")
 
-        if bandera==0:
-            print("\n\n\n\n\n\t\t\tÉxito al generar el archivo word con los hash!!")
+        if lang== 7:
+            if bandera==0:
+                print("\n\n\n\n\n\t\t\tÉxito al generar el archivo word con los hash!!")
 
-        elif bandera==1:
-            print("\n\n\n\n\n\t\t\tSaliendo del programa, recuerda llenar los requisitos previos")
-            print("\t\t\tComo el archivo excel para que obtenga todas las rutas necesarias y los datos necesarios.")
+            elif bandera==1:
+                print("\n\n\n\n\n\t\t\tSaliendo del programa, recuerda llenar los requisitos previos")
+                print("\t\t\tComo el archivo excel para que obtenga todas las rutas necesarias y los datos necesarios.")
 
-        elif bandera==2:
-            print("\n\n\n\n\n\t\t\tPor favor, ingresa una de las opciones que se te pide ingresar")
-            print("\t\t\tRegresando a la vista anterior")
+            elif bandera==2:
+                print("\n\n\n\n\n\t\t\tPor favor, ingresa una de las opciones que se te pide ingresar")
+                print("\t\t\tRegresando a la vista anterior")
+
+        elif lang== 8:
+            if bandera== 0:
+                print("\n\n\n\n\n\t\t\tSuccess in generating the word file with the hash!!")
+
+            elif bandera== 1:
+                print("\n\n\n\n\n\t\t\tWhen leaving the program, remember to fill the prerequisites")
+                print("\t\t\tlike the excel file so that you get all the necessary paths and the necessary data.")
+
+            elif bandera== 2:
+                print("\n\n\n\n\n\t\t\tPlease, enter one of the options that you are asked to enter")
+                print("\t\t\tReturning to the previous view")
 
         time.sleep(4)
 
@@ -50,3 +63,24 @@ class Menus():
                 print("\tPor favor, ingresa el nombre del archivo con la extensión (.xlsx)")
                 print("\tRegresando al menú anterior...")
         return rutita
+
+    def Language(self):
+        salir= False
+
+        while not salir:
+            os.system("cls")
+            try:
+                print("\n\n\n\n\t\t\t\t1= Español, 2= English: ")
+                leng= int(input())
+                if leng== 1:
+                    leng= 7#Siete español y 8 inglés.
+                elif leng== 2:
+                    leng= 8
+                salir= True
+
+            except Exception as e:
+                print(f"Ocurrió el ERROR/There is an ERROR: {e}")
+                print("Recuerda que debes ingresar un 1 o un 2 nada más")
+                print("Remeber that you need to enter a number 1 or 2 only")
+        
+        return leng
--- PrincipalHash.py
+++ PrincipalHash.py
@@ -7,25 +7,22 @@ op= 0
 salir= False
 
 try:
+    Lang= Vistas.Language()
     while not salir:
-    #Menú en caso de que se necesitase, archivo Pantallas
-    #Ingresar también la validación del número entero, archivo Pantallas, este no va pero es bueno que lo tenga en caso hayan-
-    #menús más grandes
-
         op= Vistas.SeguroContinuar()
 
         if op==1:
             RutaEx= Vistas.RutaEx()
             SacHash.hash(RutaEx)
-            Vistas.MenuPrin(0)
+            Vistas.MenuPrin(0, Lang)
             salir= True
 
         elif op==2:
-            Vistas.MenuPrin(1)
+            Vistas.MenuPrin(1, Lang)
             salir= True
         
         else:
-            Vistas.MenuPrin(2)
+            Vistas.MenuPrin(2, Lang)
 
 except Exception as e:
     os.system("cls")
